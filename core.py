8dd35c005291017df3d78a43968d6206015c334e
Gravity cat <29692485+nekogravitycat@users.noreply.github.com>
2022-06-19 14:00:40 +0000

    Applied early return style in core.py

--- core.py
+++ core.py
@@ -7,30 +7,31 @@ class status:
 		self.ok: bool = ok
 		self.detail: str = detail
 
+
 class url:
 	def is_legal(key: str) -> bool:
 		illeagal: list = ["login", "utr"]
 		return re.match(r"^[\w-]+$", key) and not key in illeagal
+
 	
-	def add(dest: str, orig: str) -> str:
-		if(not dest or not orig):
-			return status(False, "empty input")
+	def add(shorten: str, orig: str) -> str:
+		if(not shorten or not orig):
+			return status(False, "empty orig or shorten")
+
+		if(not url.is_legal(shorten)):
+			return status(False, "illegal shorten url")
 	
 		#adds https protocol if not exists
 		if(not orig.startswith("https://") and not orig.startswith("http://")):
 			orig = f"https://{orig}"
-	
-		if(url.is_legal(dest)):
-			db[dest] = orig
-			return status(True, "ok")
-		
-		else:
-			return status(False, "illegal key name")	
+
+		db[shorten] = orig
+		return status(True, "ok")
+
 	
 	def delete(code: str):
-		if(code in db.keys()):
-			del db[code]
-			return status(True, "ok")
-		
-		else:
+		if(not code in db.keys()):
 			return status(False, "key does not exist")
+			
+		del db[code]
+		return status(True, "ok")
